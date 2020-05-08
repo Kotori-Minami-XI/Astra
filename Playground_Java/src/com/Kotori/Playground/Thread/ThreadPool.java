f46b88826f866712cea46a3d471fa1dfd5f71eb5
Kotori-Minami-XI <543566812@qq.com>
2020-05-08 17:13:28 +0800

    [Task] Playground_Java

--- Playground_Java/src/com/Kotori/Playground/Thread/ThreadPool.java
+++ Playground_Java/src/com/Kotori/Playground/Thread/ThreadPool.java
@@ -1,13 +1,54 @@
 package com.Kotori.Playground.Thread;
 
 
+import org.junit.Test;
+
 import java.util.concurrent.*;
 
 public class ThreadPool {
-//    ThreadPoolExecutor service = new ThreadPoolExecutor(5, 200,
-//            0L, TimeUnit.MILLISECONDS,
-//            new LinkedBlockingQueue<>(1024),
-//            new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build(),
-//            new ThreadPoolExecutor.AbortPolicy());
+    @Test
+    public void testThreadPool() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 6,
+                60L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(1024),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
 
+        for (int i = 0; i <= 10; i++)
+        {
+            Task task = new Task("Task " + i);
+            System.out.println("A new task has been added : " + task.getName());
+            executor.execute(task);
+        }
+
+        executor.shutdown();
+        while(true) {}
+    }
+}
+
+class Task implements Runnable
+{
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+        try
+        {
+            Long duration = (long) (Math.random() * 10);
+            System.out.println("Doing a task during : " + name);
+            TimeUnit.SECONDS.sleep(duration);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
