9921c8f7cd82fa6ddc4fb415b62cc6229087e205
Kotori <543566812@qq.com>
2020-05-20 22:37:14 +0800

    [task] java_playground

--- Playground_Java/src/Demo1.java
+++ Playground_Java/src/Demo1.java
@@ -1,27 +1,56 @@
 import org.junit.Test;
 
+
+
+class Counter{
+    public Integer count;
+
+    public Counter() {
+        count = 0;
+    }
+}
+
 public class Demo1 {
     @Test
-    public void test() {
-        A a1= new A();
-        A.a=1;
-        A a2 = new A();
-        System.out.println(A.a);
-        A b = new B();
-        System.out.println(B.a);
-        b.f();
+    public void test() throws InterruptedException {
+        Counter counter1 =new Counter();
+        Counter counter2 =new Counter();
+
+        Thread t1 = new Thread(new Adder(counter1, "t1"));
+        Thread t2 = new Thread(new Adder(counter2,"t2"));
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
     }
 }
 
-class A {
-    static Integer a = 5;
-    static void f (){
-        System.out.println("A");
+
+class Adder implements Runnable{
+    private String name;
+    private Counter counter;
+
+    public Adder(Counter counter, String name) {
+        this.counter = counter;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (Counter.class) {
+                try {
+                    this.counter.count++;
+                    System.out.println(this.name + " count=" + this.counter.count);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
 
-class B extends A {
-    static void f (){
-        System.out.println("B");
-    }
-}
+
--- Playground_Java/src/com/Kotori/Playground/Thread/Wait_Notify.java
+++ Playground_Java/src/com/Kotori/Playground/Thread/Wait_Notify.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class Wait_Notify {
 
-    @Test
-    public void test() throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
         threads.add(new Thread(new OilMaker("OilMaker1")));
+        threads.add(new Thread(new OilMaker("OilMaker2")));
 
         threads.add(new Thread(new Car("Car1")));
         threads.add(new Thread(new Car("Car2")));
@@ -27,25 +27,6 @@ public class Wait_Notify {
 
 class OilGoods {
     static volatile Integer Oil = 5;
-
-    public void consume(String name) throws InterruptedException {
-        while (OilGoods.Oil <= 0) {
-            wait();
-        }
-        OilGoods.Oil++;
-        System.out.println(name + "消耗了Oil，还剩下" + OilGoods.Oil);
-        notifyAll();
-    }
-
-    public void produce(String name) throws InterruptedException {
-        while (OilGoods.Oil >= 20) {
-            wait();
-        }
-        OilGoods.Oil++;
-        System.out.println(name + "生产了Oil，还剩下" + OilGoods.Oil);
-        notifyAll();
-    }
-
 }
 
 class Car implements Runnable {
@@ -66,11 +47,17 @@ class Car implements Runnable {
     @Override
     public void run() {
         while (true) {
-            try {
-                OilGoods.consume(this.getName());
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            synchronized (OilGoods.class) {
+                while (OilGoods.Oil <= 0) {
+                    try {
+                        OilGoods.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                OilGoods.Oil--;
+                System.out.println(name + "消耗了Oil，还剩下" + OilGoods.Oil);
+                OilGoods.class.notifyAll();
             }
         }
     }
@@ -93,13 +80,19 @@ class OilMaker implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
-            try {
-                OilGoods.produce(this.getName());
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try{
+            while (true) {
+                synchronized (OilGoods.class) {
+                    while (OilGoods.Oil >= 20) {
+                        OilGoods.class.wait();
+                    }
+                    OilGoods.Oil++;
+                    System.out.println(name + "生产了Oil，还剩下" + OilGoods.Oil);
+                    OilGoods.class.notifyAll();
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
